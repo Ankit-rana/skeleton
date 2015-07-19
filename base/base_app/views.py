7aa4f486318f6a79da63f4cfa95fec7f3c2c3fd8
root <ankit9.7.91@gmail.com>
2015-07-19 23:08:12 +0530

    template_init

--- base/base_app/views.py
+++ base/base_app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.template import RequestContext, loader
 from django.http import HttpResponse
 
 # Create your views here.
-def home(reuest):
-	return HttpResponse("<h2>#Welcome</h2>")
+def home(request):
+	template = loader.get_template('base_app/home.html')
+	return HttpResponse(template)
 
 def index(request):
         return HttpResponse("<h2>#ZOMBIE</h2>")
